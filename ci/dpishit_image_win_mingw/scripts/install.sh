cdeed1f48a8b4c4eb83667d6b4b2419f850d91e7
nullgemm <nullgemm@mailbox.org>
2023-08-07 20:58:22 +0200

    update windows ci install script comments

--- ci/dpishit_image_win_mingw/scripts/install.sh
+++ ci/dpishit_image_win_mingw/scripts/install.sh
@@ -3,6 +3,10 @@
 # git						cloning the repo
 # bash						running the build scripts
 # ninja  					building the binaries
+# mingw-w64-binutils
+# mingw-w64-gcc
+# mingw-w64-headers
+# mingw-w64-crt
 
 apk add --no-cache \
 	git \
